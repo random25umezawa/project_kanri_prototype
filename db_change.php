c104e1e683165560ab0560b772df07b5f069b087
Umezawa Toshifumi <bday7125@gmail.com>
2017-08-20 11:02:55 +0900

    aiの設定まで。

--- db_change.php
+++ db_change.php
@@ -70,6 +70,26 @@ function db_addDB($pdo,$pre_table_cols,$table_cols) {
 				print($query.PHP_EOL);
 				$pdo->query($query);
 				continue;
+			}else {
+				/*
+				print("------------------");
+				print($col_name);
+				var_dump($col["Key"]);
+				var_dump($pre_table_cols[$table_name]["column"][$col_name]["Key"]);
+				*/
+				if($col["Key"]=="PRI" && $pre_table_cols[$table_name]["column"][$col_name]["Key"]=="") {
+					$query = sprintf('alter table %s add primary key (%s)',$table_name,$col_name);
+					$pdo->query($query);
+					print($col_name."<br>");
+				}
+				$col_type = $col["Type"];
+				if(array_key_exists("Size",$col)) {
+					$col_type .= "(".$col["Size"].")";
+				}
+				if($col["Extra"]=="auto_increment" && $pre_table_cols[$table_name]["column"][$col_name]["Extra"]=="") {
+					$query = sprintf('alter table %s change %s %s %s not null auto_increment',$table_name,$col_name,$col_name,$col_type);
+					$pdo->query($query);
+				}
 			}
 			foreach($col as $type => $value) {
 				//
